c528001da592b7cd15e1b73e7c0d2847a259c69d
wanjau2 <eugenewanjau@gmail.com>
2023-10-11 22:48:19 +0300

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - function that prints the alphabet in lower case 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (i = 'a'; a <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
