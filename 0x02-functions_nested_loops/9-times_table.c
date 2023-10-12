7268632f6a40c627ed3b791b1397139840b7c991
wanjau2 <eugenewanjau@gmail.com>
2023-10-12 09:47:56 +0300

    debug

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,10 +15,10 @@ void times_table(void)
 		{
 			result = i * j;
 			if (j == 0)
-				printf("%d",  result);
+				printf("%d, ",  result);
 			else
 			{
-				printf("%2d" , result);
+				printf("%2d", result);
 				if (j != 9)
 					printf(",");
 			}
