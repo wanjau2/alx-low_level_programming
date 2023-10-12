c67f26a38d57c66c70229f31b0068b67ec1eabcf
wanjau2 <eugenewanjau@gmail.com>
2023-10-12 18:57:25 +0300

    multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function that print the multiplication
+ *
+ * @a: The character to print
+ * @b: The character to print
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
