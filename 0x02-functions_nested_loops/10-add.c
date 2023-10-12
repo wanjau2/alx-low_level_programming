e4cb4da8abd267eabc31bec1b91be697232e98d5
wanjau2 <eugenewanjau@gmail.com>
2023-10-12 10:04:19 +0300

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function to add two intergers
+ *
+ * @a: The character to print
+ * @b: The character to print
+ *
+ *
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
