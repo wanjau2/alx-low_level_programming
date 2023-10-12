a8159fe49c14222ed20068d5b22b1494fe245ae8
wanjau2 <eugenewanjau@gmail.com>
2023-10-12 19:30:44 +0300

    using the continue fnction

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints all integers except 2 and 4
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
