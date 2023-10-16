dfb715511e2020897d87f5d4fed75d56bddf8036
wanjau2 <eugenewanjau@gmail.com>
2023-10-16 18:34:29 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes the value of the adress to 98
+ *
+ * @n: The character to print
+ *
+ * Return: Always n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
