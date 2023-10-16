3afe83974de8fdc5aaead7f3bbc8ef9e1f24ad00
wanjau2 <eugenewanjau@gmail.com>
2023-10-17 01:21:39 +0300

    I can only go one way .i have not got a reverse car

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,39 +3,13 @@
  * print_rev - prints the string in reverse
  * @s: string to be printed
  * Return: void
- *
- *void print_rev(char *s)
- *{
- *	int i;
- *	int count = 0;
- *
- *	for (i = 0; s[i] != '\0'; i++)
- *		count++;
- *	for (i = count - 1; i >= 0; i++)
- *		_putchar(s[i]);
- *
- * _putchar('\n');
- *}
- *
  */
 void print_rev(char *s)
 {
 	int i, j;
-	char temp;
 
-	i = 0;
 	j = strlen(s) - 1;
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
-	for (i = 0; i != '\0'; i++)
-	{
+	for (i = j; i >= 0; i--)
 		_putchar(s[i]);
-	}
 	_putchar('\n');
 }
