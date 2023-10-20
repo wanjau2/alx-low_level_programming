1dfa89e5533175209d7bba2a9f3206cee4550e8a
wanjau2 <eugenewanjau@gmail.com>
2023-10-20 03:32:19 +0300

    rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,14 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int t;
 
-	for (i = n - 1; i >= 0; i--)
+	for (i = 0; i < n--; i++)
 	{
-		printf("%d", a[i]);
-		if (i != 0)
-			printf(", ");
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
-	printf("\n");
 }
+
+
