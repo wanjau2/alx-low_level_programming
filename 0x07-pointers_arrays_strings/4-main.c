f1544181a8c16333925b4aab64bff8afe6ba460c
wanjau2 <eugenewanjau@gmail.com>
2023-10-24 00:56:38 +0300

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strpbrk(s, f);
+	printf("%s\n", t);
+	return (0);
+}
