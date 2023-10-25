3de168fd3baa61e47fbc87178a34a26a8061f8b5
wanjau2 <eugenewanjau@gmail.com>
2023-10-26 01:15:40 +0300

    betty dec

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,6 +10,12 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
+/**
+ * actual_sqrt_recursion - check the sqrt
+ * @n: the charcter
+ * @i: the other
+ * Return: Always 0.
+ */
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
