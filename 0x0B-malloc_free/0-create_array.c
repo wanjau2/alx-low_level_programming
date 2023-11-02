0f781f5e240691f0b15146a01a90e3279023dd8e
wanjau2 <eugenewanjau@gmail.com>
2023-11-02 05:17:07 +0300

    strdup

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - create a size 
+ * create_array - create a size
  * @size:size
  * @c:to assign
  * Return: Always 0.
@@ -9,19 +9,11 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
-			
 	unsigned int i;
-			
 
-			
 	str = malloc(sizeof(char) * size);
-			
 	if (size == 0 || str == NULL)
-				
 		return (NULL);
-			
-
-			
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
