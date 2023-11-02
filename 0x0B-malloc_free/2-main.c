d257b77d4736a19508a15f5ad30822890aa0faa5
wanjau2 <eugenewanjau@gmail.com>
2023-11-02 05:28:30 +0300

    concat

---
+++ 0x0B-malloc_free/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	char *s;
+
+	s = str_concat("Betty ", "Holberton");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	return (0);
+}
