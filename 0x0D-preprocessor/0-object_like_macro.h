ebdad897eebb6b569b8293e2893613770fc6ad6a
wanjau2 <eugenewanjau@gmail.com>
2023-11-07 04:56:54 +0300

    PI

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#define object_like_macro_H
+#ifdef object_like_macro_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 1024
+
+
+
+
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#define PI_H
+#ifdef PI_H
+
+#include <stdio.h>
+#define PI 3.14159265359
+
+#endif
