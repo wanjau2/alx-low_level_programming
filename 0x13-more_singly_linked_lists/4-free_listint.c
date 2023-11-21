97834a003f8192da3175c08dc84a42b68138a7a8
wanjau2 <eugenewanjau@gmail.com>
2023-11-22 00:56:31 +0300

    frree lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - frees the linked list
+ * @head: listint_t list be freed
+ *
+ * Return: NULL
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
